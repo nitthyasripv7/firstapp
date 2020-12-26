53d3436685d0754bc6a2472d4135f6bd016130dc
ethil <nidinkrish@gmail.com>
2020-12-26 13:41:27 +0530

    lunch check changes

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-(function(){
-    'use strict';
- //x="hello";
-
-    angular.module('myfirstapp',[])
-
-    .controller('myfirstcontroller',function(){
-
-    });
-
-})();
+(function () {
+  'use strict';
+  
+    angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+  
+    LunchCheckController.$inject = ['$scope'];
+  
+    function LunchCheckController($scope) {
+      $scope.lunchItems = '';
+      $scope.messageStyle = { 'color': 'red' };
+      $scope.textboxStyle = { 'border-color': 'red' };
+      $scope.checkLunch = function () {
+        var numItems = $scope.lunchItems
+          .split(',')
+          .filter(x => x.trim() != '')
+          .length;
+  
+        if (numItems == 0) {
+          $scope.message = 'Please enter data first';
+          $scope.messageStyle = { 'color': 'red' };
+          $scope.textboxStyle = { 'border-color': 'red' };
+        } else if (numItems <= 3) {
+          $scope.message = 'Enjoy!';
+          $scope.messageStyle = { 'color': 'green' };
+          $scope.textboxStyle = { 'border-color': 'green' };
+        } else {
+          $scope.message = 'Too much!';
+          $scope.messageStyle = { 'color': 'green' };
+          $scope.textboxStyle = { 'border-color': 'green' };
+        }
+      }
+    }
+  
+  
+  })();
